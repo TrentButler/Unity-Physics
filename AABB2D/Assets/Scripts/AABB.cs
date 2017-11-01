3fef48919db918fd95cec0d9afe418f6cd86653d
Trent Butler <s178061@students.aie.edu.au>
2017-11-01 17:54:55 -0500

    aabb practice

---
+++ AABB2D/Assets/Scripts/AABB.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AABB
+{
+    public void Init(Vector2 position, float size)
+    {
+        //XMIN = POSITION.X -= SIZE
+        //XMAX = POSITION.X += SIZE
+        //YMIN = POSITION.Y -= SIZE
+        //YMAX = POSITION.Y += SIZE
+
+        Position = position;
+        _size = size;
+        Min.x = position.x - size;
+        Min.y = position.y - size;
+        Max.x = position.x + size;
+        Max.y = position.y + size;
+    }
+
+    public void Update(Vector2 position)
+    {
+        Position = position;
+
+        Min.x = position.x - _size;
+        Min.y = position.y - _size;
+        Max.x = position.x + _size;
+        Max.y = position.y + _size;
+    }
+
+    public Vector2 Position = Vector2.zero;
+    public Vector2 Min = Vector2.zero;
+    public Vector2 Max = Vector2.zero;
+    private float _size;
+}
+
+public class Utilities
+{
+    public bool TestOverLap(AABB col1, AABB col2)
+    {
+        if (col1.Max.x >= col2.Min.x && col1.Max.y >= col2.Min.y)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ AABB2D/Assets/Scripts/AABBBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AABBBehaviour : MonoBehaviour {
+
+    public AABB collider1 = new AABB();
+    public AABB collider2 = new AABB();
+    public Utilities util = new Utilities();
+
+    public bool Collision = false;
+    public Vector2 Collider1BOXMAX = Vector2.zero;
+    public Vector2 Collider1BOXMIN = Vector2.zero;
+
+    public Vector2 Collider2BOXMAX = Vector2.zero;
+    public Vector2 Collider2BOXMIN = Vector2.zero;
+
+    private Transform colGO1;
+    private Transform colGO2;
+
+    private Vector2 col1Pos;
+    private Vector2 col2Pos;
+
+    // Use this for initialization
+    void Start ()
+    {
+        colGO1 = GameObject.FindGameObjectWithTag("col1").transform;
+        colGO2 = GameObject.FindGameObjectWithTag("col2").transform;
+
+        col1Pos = new Vector2(colGO1.transform.position.x, colGO1.transform.position.y);
+        col2Pos = new Vector2(colGO2.transform.position.x, colGO2.transform.position.y);
+
+        collider1.Init(col1Pos, 1);
+        collider2.Init(col2Pos, 1);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        col1Pos = new Vector2(colGO1.transform.position.x, colGO1.transform.position.y);
+        col2Pos = new Vector2(colGO2.transform.position.x, colGO2.transform.position.y);
+
+        collider1.Update(col1Pos);
+        collider2.Update(col2Pos);
+
+        Collider1BOXMAX = collider1.Max;
+        Collider1BOXMIN = collider1.Min;
+
+        Collider2BOXMAX = collider2.Max;
+        Collider2BOXMIN = collider2.Min;
+
+        Collision = util.TestOverLap(collider1, collider2);
+    }
+}
