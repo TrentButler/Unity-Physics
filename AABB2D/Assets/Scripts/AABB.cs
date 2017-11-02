230f6aa63db0179e7f64307dda2cfc0011300e26
Trent Butler <s178061@students.aie.edu.au>
2017-11-02 17:53:37 -0500

    sort and sweep practice

--- AABB2D/Assets/Scripts/AABB.cs
+++ AABB2D/Assets/Scripts/AABB.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AABB
 {
-    public void Init(Vector2 position, float size)
+    public void Init(string name, Vector2 position, float size)
     {
         //XMIN = POSITION.X -= SIZE
         //XMAX = POSITION.X += SIZE
         //YMIN = POSITION.Y -= SIZE
         //YMAX = POSITION.Y += SIZE
 
+        Name = name;
         Position = position;
         _size = size;
         Min.x = position.x - size;
@@ -29,20 +31,85 @@ public class AABB
         Max.y = position.y + _size;
     }
 
+    public string Name = "";
     public Vector2 Position = Vector2.zero;
     public Vector2 Min = Vector2.zero;
     public Vector2 Max = Vector2.zero;
-    private float _size;
+    public float _size;
 }
 
+public class ColPair
+{
+    private List<AABB> pair = new List<AABB>();
+
+    public void AddPair(AABB one, AABB two)
+    {
+        if(pair.Count <= 0)
+        {
+            pair.Add(one);
+            pair.Add(two);
+        }
+    }
+
+    public List<AABB> GetPair()
+    {
+        return pair;
+    }
+}
+
+[System.Serializable]
 public class Utilities
 {
+    
+    private List<AABB> axisList = new List<AABB>();
+    public List<ColPair> pairs = new List<ColPair>();
+
     public bool TestOverLap(AABB col1, AABB col2)
     {
-        if (col1.Max.x >= col2.Min.x && col1.Max.y >= col2.Min.y)
+        var d1x = col2.Min.x - col1.Max.x;
+        var d1y = col2.Min.y - col1.Max.y;
+
+        var d2x = col1.Min.x - col2.Max.x;
+        var d2y = col1.Min.y - col2.Max.y;
+
+        if (d1x > 0 || d1y > 0)
         {
-            return true;
+            return false;
         }
-        return false;
+
+        if (d2x > 0 || d2y > 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void SortandSweep(List<AABB> objectsInScene)
+    {
+        var sortedObjects = objectsInScene.OrderBy(x => x.Min.x).ToList(); //SORT ALL OBJECTS IN SCENE BY THE MINIMUM X        
+
+        axisList = sortedObjects; //STORE THE SORTED OBJECTS
+
+        List<AABB> activeList = new List<AABB>(); //CREATE A NEW LIST OF AABB
+
+        for(int i = 0; i < axisList.Count - 1; i++)
+        {
+            activeList.Add(axisList[i]); // ADD THE FIRST ITEM FROM THE AXISLIST
+
+            if (axisList[i + 1].Min.x > activeList[i].Max.x) //IF NEWITEM.RIGHT > CURRENTITEM.LEFT
+            {
+                activeList.Remove(activeList[i]); //REMOVE THE CURRENT ITEM
+            }
+
+            else
+            {
+                ColPair newPair = new ColPair();
+                newPair.AddPair(activeList[i], axisList[i + 1]);
+                pairs.Add(newPair); //REPORT A PAIR
+
+                activeList.Add(axisList[i + 1]); //ADD NEWITEM TO THE ACTIVE LIST
+            }
+        }
     }
 }
--- AABB2D/Assets/Scripts/AABBBehaviour.cs
+++ AABB2D/Assets/Scripts/AABBBehaviour.cs
@@ -3,52 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AABBBehaviour : MonoBehaviour {
-
+    
     public AABB collider1 = new AABB();
     public AABB collider2 = new AABB();
+    public AABB collider3 = new AABB();
+    public AABB collider4 = new AABB();
+
     public Utilities util = new Utilities();
 
-    public bool Collision = false;
-    public Vector2 Collider1BOXMAX = Vector2.zero;
-    public Vector2 Collider1BOXMIN = Vector2.zero;
+    private List<AABB> objects = new List<AABB>();
+    public List<AABB> colliderPairs = new List<AABB>();
 
-    public Vector2 Collider2BOXMAX = Vector2.zero;
-    public Vector2 Collider2BOXMIN = Vector2.zero;
+    public bool Collision = false;
+    //public Vector2 Collider1BOXMAX = Vector2.zero;
+    //public Vector2 Collider1BOXMIN = Vector2.zero;
+    //public Vector2 Collider2BOXMAX = Vector2.zero;
+    //public Vector2 Collider2BOXMIN = Vector2.zero;
 
     private Transform colGO1;
     private Transform colGO2;
+    private Transform colGO3;
+    private Transform colGO4;
 
     private Vector2 col1Pos;
     private Vector2 col2Pos;
-
-    // Use this for initialization
+    private Vector2 col3Pos;
+    private Vector2 col4Pos;
+    
     void Start ()
     {
         colGO1 = GameObject.FindGameObjectWithTag("col1").transform;
         colGO2 = GameObject.FindGameObjectWithTag("col2").transform;
+        colGO3 = GameObject.FindGameObjectWithTag("col3").transform;
+        colGO4 = GameObject.FindGameObjectWithTag("col4").transform;
 
         col1Pos = new Vector2(colGO1.transform.position.x, colGO1.transform.position.y);
         col2Pos = new Vector2(colGO2.transform.position.x, colGO2.transform.position.y);
+        col3Pos = new Vector2(colGO3.transform.position.x, colGO3.transform.position.y);
+        col4Pos = new Vector2(colGO4.transform.position.x, colGO4.transform.position.y);
 
-        collider1.Init(col1Pos, 1);
-        collider2.Init(col2Pos, 1);
+        collider1.Init("A", col1Pos, 1);
+        collider2.Init("B", col2Pos, 1);
+        collider3.Init("C", col3Pos, 1);
+        collider4.Init("D", col4Pos, 1);
+        
+        objects.Add(collider1);
+        objects.Add(collider2);
+        objects.Add(collider3);
+        objects.Add(collider4);
     }
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
         col1Pos = new Vector2(colGO1.transform.position.x, colGO1.transform.position.y);
         col2Pos = new Vector2(colGO2.transform.position.x, colGO2.transform.position.y);
+        col3Pos = new Vector2(colGO3.transform.position.x, colGO3.transform.position.y);
+        col4Pos = new Vector2(colGO4.transform.position.x, colGO4.transform.position.y);
 
         collider1.Update(col1Pos);
         collider2.Update(col2Pos);
+        collider3.Update(col3Pos);
+        collider4.Update(col4Pos);
 
-        Collider1BOXMAX = collider1.Max;
-        Collider1BOXMIN = collider1.Min;
+        //Collider1BOXMAX = collider1.Max;
+        //Collider1BOXMIN = collider1.Min;
 
-        Collider2BOXMAX = collider2.Max;
-        Collider2BOXMIN = collider2.Min;
+        //Collider2BOXMAX = collider2.Max;
+        //Collider2BOXMIN = collider2.Min;
 
         Collision = util.TestOverLap(collider1, collider2);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            util.SortandSweep(objects);
+        }
     }
 }
