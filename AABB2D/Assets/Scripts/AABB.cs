e149f8988c1bf7be168fe3ae4f46d8f8de12516e
Trent Butler <s178061@students.aie.edu.au>
2017-12-06 12:38:02 -0600

    aabb zAxis sort and sweep

--- AABB2D/Assets/Scripts/AABB.cs
+++ AABB2D/Assets/Scripts/AABB.cs
@@ -3,269 +3,270 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class aaBB : ScriptableObject
+namespace Trent
 {
-    public void _Init(string name, Vector2 position, float size)
+    [System.Serializable]
+    public class aaBB : ScriptableObject
     {
-        //XMIN = POSITION.X -= SIZE
-        //XMAX = POSITION.X += SIZE
-        //YMIN = POSITION.Y -= SIZE
-        //YMAX = POSITION.Y += SIZE
-
-        _name = name;
-        _position = position;
-        _size = size;
-        min.x = position.x - size;
-        min.y = position.y - size;
-        max.x = position.x + size;
-        max.y = position.y + size;
-    }
-
-    public void _Update(Vector2 position)
-    {
-        _position = position;
-
-        min.x = position.x - Size;
-        min.y = position.y - Size;
-        max.x = position.x + Size;
-        max.y = position.y + Size;
-    }
-
-    public string Name { get { return _name; } }
-    private string _name;
-
-    public Vector2 Position { get { return _position; } }
-    private Vector2 _position;
-
-    public Vector2 Min { get { return min; } }
-    private Vector2 min;
-
-    public Vector2 Max { get { return max; } }
-    private Vector2 max;
-
-    public float Size { get { return _size; } }
-    private float _size;
-}
-
-public class ColPair
-{
-    private List<aaBB> pair = new List<aaBB>();
-
-    public void AddPair(aaBB one, aaBB two)
-    {
-        if (pair.Count <= 0)
+        public void _Init(string name, Vector3 position, float size)
         {
-            pair.Add(one);
-            if (two == one)
-            {
-                return;
-            }
-            pair.Add(two);
+            //XMIN = POSITION.X -= SIZE
+            //XMAX = POSITION.X += SIZE
+            //YMIN = POSITION.Y -= SIZE
+            //YMAX = POSITION.Y += SIZE
+
+            _name = name;
+            _position = position;
+            _size = size;
+            min.x = position.x - size;
+            min.y = position.y - size;
+            min.z = position.z - size;
+
+            max.x = position.x + size;
+            max.y = position.y + size;
+            max.z = position.z + size;
         }
+
+        public void _Update(Vector3 position)
+        {
+            _position = position;
+
+            min.x = position.x - Size;
+            min.y = position.y - Size;
+            min.z = position.z - Size;
+
+            max.x = position.x + Size;
+            max.y = position.y + Size;
+            max.z = position.z + Size;
+        }
+
+        public string Name { get { return _name; } }
+        private string _name;
+
+        public Vector3 Position { get { return _position; } }
+        private Vector3 _position;
+
+        public Vector3 Min { get { return min; } }
+        private Vector3 min;
+
+        public Vector3 Max { get { return max; } }
+        private Vector3 max;
+
+        public float Size { get { return _size; } }
+        private float _size;
     }
 
-    public bool ComparePair(ColPair other)
+    public class ColPair
     {
-        var one = other.GetPair()[0];
-        var two = other.GetPair()[1];
+        private List<aaBB> pair = new List<aaBB>();
 
-        if(pair != null && pair.Count > 1)
+        public void AddPair(aaBB one, aaBB two)
         {
-            var three = pair[0];
-            var four = pair[1];
-
-            if(one == three || one == four)
+            if (pair.Count <= 0)
             {
-                if(two == three || two == four)
+                pair.Add(one);
+                if (two == one)
                 {
-                    return true;
+                    return;
                 }
+                pair.Add(two);
             }
-
         }
-        return false;
-    }
 
-    public List<aaBB> GetPair()
-    {
-        return pair;
-    }
-
-    public string GetPairAsString()
-    {
-        if (pair.Count > 0)
+        public bool ComparePair(ColPair other)
         {
-            string sPair = "";
-            for (int i = 0; i < pair.Count; i++)
+            var one = other.GetPair()[0];
+            var two = other.GetPair()[1];
+
+            if (pair != null && pair.Count > 1)
             {
-                sPair += (pair[i].Name + ",");
+                var three = pair[0];
+                var four = pair[1];
+
+                if (one == three || one == four)
+                {
+                    if (two == three || two == four)
+                    {
+                        return true;
+                    }
+                }
+
             }
-            return sPair;
-        }
-        return "";
-    }
-}
-
-public class Utilities
-{
-    private List<aaBB> XaxisList = new List<aaBB>();
-    private List<aaBB> YaxisList = new List<aaBB>();
-    //private List<aaBB> ZaxisList = new List<aaBB>();
-
-    public List<ColPair> Xpairs = new List<ColPair>();
-    public List<ColPair> Ypairs = new List<ColPair>();
-    public List<ColPair> Zpairs = new List<ColPair>();
-
-    public List<string> agentsColliding = new List<string>();
-
-    public bool TestOverLap(aaBB col1, aaBB col2)
-    {
-        var d1x = col2.Min.x - col1.Max.x;
-        var d1y = col2.Min.y - col1.Max.y;
-
-        var d2x = col1.Min.x - col2.Max.x;
-        var d2y = col1.Min.y - col2.Max.y;
-
-        if (d1x > 0 || d1y > 0)
-        {
             return false;
         }
 
-        if (d2x > 0 || d2y > 0)
+        public List<aaBB> GetPair()
         {
-            return false;
+            return pair;
         }
 
-        return true;
-    }
-
-    public void TestCollision()
-    {
-        agentsColliding = new List<string>();
-        //IF THERE ARE DUPLICATE PAIRS IN BOTH AXIS, THERE IS COLLISION
-        for(int i = 0; i < Xpairs.Count; i++)
+        public string GetPairAsString()
         {
-            for(int j = 0; j < Ypairs.Count; j++)
+            if (pair.Count > 0)
             {
-                if(Xpairs[i].ComparePair(Ypairs[j]) == true)
+                string sPair = "";
+                for (int i = 0; i < pair.Count; i++)
                 {
-                    string newPair = Xpairs[i].GetPairAsString();
-                    agentsColliding.Add(newPair);
+                    sPair += (pair[i].Name + ",");
                 }
+                return sPair;
             }
+            return "";
         }
     }
 
-    public void SortandSweep(List<aaBB> objectsInScene)
+    public class Collider
     {
-        Xpairs = new List<ColPair>();
-        Ypairs = new List<ColPair>();
-        //Zpairs = new List<ColPair>();
+        private List<aaBB> XaxisList = new List<aaBB>();
+        private List<aaBB> YaxisList = new List<aaBB>();
+        private List<aaBB> ZaxisList = new List<aaBB>();
 
-        var sortedX = objectsInScene.OrderBy(x => x.Min.x).ToList(); //SORT ALL OBJECTS IN SCENE BY THE MINIMUM X
-        var sortedY = objectsInScene.OrderBy(x => x.Min.y).ToList(); //SORT ALL OBJECTS IN SCENE BY THE MINIMUM Y
-        //var sortedZ = objectsInScene.OrderBy(x => x.Min.z).ToList(); //SORT ALL OBJECTS IN SCENE BY THE MINIMUM Z
+        public List<ColPair> Xpairs = new List<ColPair>();
+        public List<ColPair> Ypairs = new List<ColPair>();
+        public List<ColPair> Zpairs = new List<ColPair>();
 
-        XaxisList = sortedX; //STORE THE SORTED OBJECTS
-        YaxisList = sortedY; //STORE THE SORTED OBJECTS
-        //ZaxisList = sortedZ; //STORE THE SORTED OBJECTS
+        public List<string> agentsColliding = new List<string>();
 
-        List<aaBB> activeList = new List<aaBB>(); //CREATE A NEW LIST OF AABB
-
-        #region XSortAndSweep
-        for (int i = 0; i < XaxisList.Count - 1; i++)
+        public bool TestOverLap(aaBB col1, aaBB col2)
         {
-            int j = i;
-            activeList.Add(XaxisList[i]); // ADD THE FIRST ITEM FROM THE AXISLIST
+            var d1x = col2.Min.x - col1.Max.x;
+            var d1y = col2.Min.y - col1.Max.y;
 
-            //CHECK FOR THE INCREMENTOR BEING GREATER THAN THE SIZE OF ACTIVE LIST
-            if (j > activeList.Count - 1)
+            var d2x = col1.Min.x - col2.Max.x;
+            var d2y = col1.Min.y - col2.Max.y;
+
+            if (d1x > 0 || d1y > 0)
             {
-                j = 0;
-            }
-            if (XaxisList[i + 1].Min.x > activeList[j].Max.x) //IF NEWITEM.RIGHT > CURRENTITEM.LEFT
-            {
-                activeList.Remove(activeList[j]); //REMOVE THE CURRENT ITEM
+                return false;
             }
 
-            else
+            if (d2x > 0 || d2y > 0)
             {
-                ColPair newPair = new ColPair();
-                newPair.AddPair(activeList[j], XaxisList[i + 1]);
-                if (Xpairs.Contains(newPair) == false) //DO NOT ADD PAIR IF ALREADY IN THE 'PAIRS' LIST
+                return false;
+            }
+
+            return true;
+        }
+
+        //NEEDS WORK
+        public void TestCollision()
+        {
+            agentsColliding = new List<string>();
+            //IF THERE ARE DUPLICATE PAIRS IN ALL THREE AXIES, THERE IS COLLISION
+
+            //12:35 - CHECK THE X,Y,Z AXISLISTs FOR DUPLICATE PAIRS
+        }
+
+        public void SortandSweep(List<aaBB> objectsInScene)
+        {
+            Xpairs = new List<ColPair>();
+            Ypairs = new List<ColPair>();
+            Zpairs = new List<ColPair>();
+
+            var sortedX = objectsInScene.OrderBy(x => x.Min.x).ToList(); //SORT ALL OBJECTS IN SCENE BY THE MINIMUM X
+            var sortedY = objectsInScene.OrderBy(x => x.Min.y).ToList(); //SORT ALL OBJECTS IN SCENE BY THE MINIMUM Y
+            var sortedZ = objectsInScene.OrderBy(x => x.Min.z).ToList(); //SORT ALL OBJECTS IN SCENE BY THE MINIMUM Z
+
+            XaxisList = sortedX; //STORE THE SORTED OBJECTS
+            YaxisList = sortedY; //STORE THE SORTED OBJECTS
+            ZaxisList = sortedZ; //STORE THE SORTED OBJECTS
+
+            List<aaBB> activeList = new List<aaBB>(); //CREATE A NEW LIST OF AABB
+
+            #region XSortAndSweep
+            for (int i = 0; i < XaxisList.Count - 1; i++)
+            {
+                int j = i;
+                activeList.Add(XaxisList[i]); // ADD THE FIRST ITEM FROM THE AXISLIST
+
+                //CHECK FOR THE INCREMENTOR BEING GREATER THAN THE SIZE OF ACTIVE LIST
+                if (j > activeList.Count - 1)
                 {
-                    Xpairs.Add(newPair); //REPORT THE PAIR
+                    j = 0;
+                }
+                if (XaxisList[i + 1].Min.x > activeList[j].Max.x) //IF NEWITEM.RIGHT > CURRENTITEM.LEFT
+                {
+                    activeList.Remove(activeList[j]); //REMOVE THE CURRENT ITEM
                 }
 
-                activeList.Add(XaxisList[i + 1]); //ADD NEWITEM TO THE ACTIVE LIST
-            }
-        }
-        #endregion
-
-        #region YSortAndSweep
-        activeList = new List<aaBB>();
-        for (int i = 0; i < YaxisList.Count - 1; i++)
-        {
-            int j = i;
-            activeList.Add(YaxisList[i]); // ADD THE FIRST ITEM FROM THE AXISLIST
-
-            //CHECK FOR THE INCREMENTOR BEING GREATER THAN THE SIZE OF ACTIVE LIST
-            if (j > activeList.Count - 1)
-            {
-                j = 0;
-            }
-
-            if (YaxisList[i + 1].Min.y > activeList[j].Max.y) //IF NEWITEM.RIGHT > CURRENTITEM.LEFT
-            {
-                activeList.Remove(activeList[j]); //REMOVE THE CURRENT ITEM
-            }
-
-            else
-            {
-                ColPair newPair = new ColPair();
-                newPair.AddPair(activeList[j], YaxisList[i + 1]);
-                if (Ypairs.Contains(newPair) == false) //DO NOT ADD PAIR IF ALREADY IN THE 'PAIRS' LIST
+                else
                 {
-                    Ypairs.Add(newPair); //REPORT THE PAIR
+                    ColPair newPair = new ColPair();
+                    newPair.AddPair(activeList[j], XaxisList[i + 1]);
+                    if (Xpairs.Contains(newPair) == false) //DO NOT ADD PAIR IF ALREADY IN THE 'PAIRS' LIST
+                    {
+                        Xpairs.Add(newPair); //REPORT THE PAIR
+                    }
+
+                    activeList.Add(XaxisList[i + 1]); //ADD NEWITEM TO THE ACTIVE LIST
+                }
+            }
+            #endregion
+
+            #region YSortAndSweep
+            activeList = new List<aaBB>();
+            for (int i = 0; i < YaxisList.Count - 1; i++)
+            {
+                int j = i;
+                activeList.Add(YaxisList[i]); // ADD THE FIRST ITEM FROM THE AXISLIST
+
+                //CHECK FOR THE INCREMENTOR BEING GREATER THAN THE SIZE OF ACTIVE LIST
+                if (j > activeList.Count - 1)
+                {
+                    j = 0;
                 }
 
-                activeList.Add(YaxisList[i + 1]); //ADD NEWITEM TO THE ACTIVE LIST
+                if (YaxisList[i + 1].Min.y > activeList[j].Max.y) //IF NEWITEM.RIGHT > CURRENTITEM.LEFT
+                {
+                    activeList.Remove(activeList[j]); //REMOVE THE CURRENT ITEM
+                }
+
+                else
+                {
+                    ColPair newPair = new ColPair();
+                    newPair.AddPair(activeList[j], YaxisList[i + 1]);
+                    if (Ypairs.Contains(newPair) == false) //DO NOT ADD PAIR IF ALREADY IN THE 'PAIRS' LIST
+                    {
+                        Ypairs.Add(newPair); //REPORT THE PAIR
+                    }
+
+                    activeList.Add(YaxisList[i + 1]); //ADD NEWITEM TO THE ACTIVE LIST
+                }
             }
+            #endregion
+
+            #region ZSortAndSweep
+            activeList = new List<aaBB>();
+            for (int i = 0; i < ZaxisList.Count - 1; i++)
+            {
+                int j = i;
+                activeList.Add(ZaxisList[i]); // ADD THE FIRST ITEM FROM THE AXISLIST
+
+                //CHECK FOR THE INCREMENTOR BEING GREATER THAN THE SIZE OF ACTIVE LIST
+                if (j > activeList.Count - 1)
+                {
+                    j = 0;
+                }
+                if (ZaxisList[i + 1].Min.z > activeList[j].Max.z) //IF NEWITEM.RIGHT > CURRENTITEM.LEFT
+                {
+                    activeList.Remove(activeList[j]); //REMOVE THE CURRENT ITEM
+                }
+
+                else
+                {
+                    ColPair newPair = new ColPair();
+                    newPair.AddPair(activeList[j], ZaxisList[i + 1]);
+                    if (Zpairs.Contains(newPair) == false) //DO NOT ADD PAIR IF ALREADY IN THE 'PAIRS' LIST
+                    {
+                        Zpairs.Add(newPair); //REPORT THE PAIR
+                    }
+
+                    activeList.Add(ZaxisList[i + 1]); //ADD NEWITEM TO THE ACTIVE LIST
+                }
+            }
+            #endregion
+
+            TestCollision();
         }
-        #endregion
-
-        #region ZSortAndSweep
-        //activeList = new List<aaBB>();
-        //for (int i = 0; i < ZaxisList.Count - 1; i++)
-        //{
-        //    int j = i;
-        //    activeList.Add(ZaxisList[i]); // ADD THE FIRST ITEM FROM THE AXISLIST
-
-        //    //CHECK FOR THE INCREMENTOR BEING GREATER THAN THE SIZE OF ACTIVE LIST
-        //    if (j > activeList.Count - 1)
-        //    {
-        //        j = 0;
-        //    }
-        //    if (ZaxisList[i + 1].Min.z > activeList[j].Max.z) //IF NEWITEM.RIGHT > CURRENTITEM.LEFT
-        //    {
-        //        activeList.Remove(activeList[j]); //REMOVE THE CURRENT ITEM
-        //    }
-
-        //    else
-        //    {
-        //        ColPair newPair = new ColPair();
-        //        newPair.AddPair(activeList[j], ZaxisList[i + 1]);
-        //        if (Zpairs.Contains(newPair) == false) //DO NOT ADD PAIR IF ALREADY IN THE 'PAIRS' LIST
-        //        {
-        //            Zpairs.Add(newPair); //REPORT THE PAIR
-        //        }
-
-        //        activeList.Add(ZaxisList[i + 1]); //ADD NEWITEM TO THE ACTIVE LIST
-        //    }
-        //}
-        #endregion
-
-        TestCollision();
     }
 }
--- AABB2D/Assets/Scripts/AABBBehaviour.cs
+++ AABB2D/Assets/Scripts/AABBBehaviour.cs
@@ -2,101 +2,121 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AABBBehaviour : MonoBehaviour
+
+namespace Trent
 {
-
-    public aaBB collider1;
-    public aaBB collider2;
-    public aaBB collider3;
-    public aaBB collider4;
-
-    public Utilities util = new Utilities();
-
-    private List<aaBB> objects = new List<aaBB>();
-    public List<string> currentOBJSColliding = new List<string>();
-    public List<string> colliderXPairs = new List<string>();
-    public List<string> colliderYPairs = new List<string>();
-
-    public bool CollisionAB = false;
-    public bool CollisionCD = false;
-
-    private Transform colGO1;
-    private Transform colGO2;
-    private Transform colGO3;
-    private Transform colGO4;
-
-    private Vector2 col1Pos;
-    private Vector2 col2Pos;
-    private Vector2 col3Pos;
-    private Vector2 col4Pos;
-
-    void Start()
+    public class AABBBehaviour : MonoBehaviour
     {
+        public aaBB collider1;
+        public aaBB collider2;
+        public aaBB collider3;
+        public aaBB collider4;
 
-        #region GameObjectTOAABB
-        colGO1 = GameObject.FindGameObjectWithTag("col1").transform;
-        colGO2 = GameObject.FindGameObjectWithTag("col2").transform;
-        colGO3 = GameObject.FindGameObjectWithTag("col3").transform;
-        colGO4 = GameObject.FindGameObjectWithTag("col4").transform;
+        public Collider util = new Collider();
 
-        col1Pos = new Vector2(colGO1.transform.position.x, colGO1.transform.position.y);
-        col2Pos = new Vector2(colGO2.transform.position.x, colGO2.transform.position.y);
-        col3Pos = new Vector2(colGO3.transform.position.x, colGO3.transform.position.y);
-        col4Pos = new Vector2(colGO4.transform.position.x, colGO4.transform.position.y);
-        #endregion
+        private List<aaBB> objects = new List<aaBB>();
+        public List<string> currentOBJSColliding = new List<string>();
+        public List<string> colliderXPairs = new List<string>();
+        public List<string> colliderYPairs = new List<string>();
+        public List<string> colliderZPairs = new List<string>();
 
-        collider1 = ScriptableObject.CreateInstance<aaBB>() as aaBB;
-        collider2 = ScriptableObject.CreateInstance<aaBB>() as aaBB;
-        collider3 = ScriptableObject.CreateInstance<aaBB>() as aaBB;
-        collider4 = ScriptableObject.CreateInstance<aaBB>() as aaBB;
+        public bool CollisionAB = false;
+        public bool CollisionCD = false;
 
-        collider1._Init("A", col1Pos, 1);
-        collider2._Init("B", col2Pos, 1);
-        collider3._Init("C", col3Pos, 1);
-        collider4._Init("D", col4Pos, 1);
+        private Transform colGO1;
+        private Transform colGO2;
+        private Transform colGO3;
+        private Transform colGO4;
 
-        objects.Add(collider1);
-        objects.Add(collider2);
-        objects.Add(collider3);
-        objects.Add(collider4);
-    }
+        private Vector2 col1Pos;
+        private Vector2 col2Pos;
+        private Vector2 col3Pos;
+        private Vector2 col4Pos;
 
-    void FixedUpdate()
-    {
-        #region UpdateColliders
-        col1Pos = new Vector2(colGO1.transform.position.x, colGO1.transform.position.y);
-        col2Pos = new Vector2(colGO2.transform.position.x, colGO2.transform.position.y);
-        col3Pos = new Vector2(colGO3.transform.position.x, colGO3.transform.position.y);
-        col4Pos = new Vector2(colGO4.transform.position.x, colGO4.transform.position.y);
-
-        collider1._Update(col1Pos);
-        collider2._Update(col2Pos);
-        collider3._Update(col3Pos);
-        collider4._Update(col4Pos);
-        #endregion
-
-        //CollisionAB = util.TestOverLap(collider1, collider2);
-        //CollisionCD = util.TestOverLap(collider3, collider4);
-
-        #region SortAndSweep
-        colliderXPairs = null;
-        colliderXPairs = new List<string>();
-
-        colliderYPairs = null;
-        colliderYPairs = new List<string>();
-
-        util.SortandSweep(objects);
-        currentOBJSColliding = util.agentsColliding;
-
-        util.Xpairs.ForEach(x =>
+        void Start()
         {
-            colliderXPairs.Add(x.GetPairAsString());
-        });
 
-        util.Ypairs.ForEach(x =>
+            #region GameObjectTOAABB
+            colGO1 = GameObject.FindGameObjectWithTag("col1").transform;
+            colGO2 = GameObject.FindGameObjectWithTag("col2").transform;
+            colGO3 = GameObject.FindGameObjectWithTag("col3").transform;
+            colGO4 = GameObject.FindGameObjectWithTag("col4").transform;
+
+            col1Pos = new Vector3(colGO1.transform.position.x, colGO1.transform.position.y, colGO1.transform.position.z);
+            col2Pos = new Vector3(colGO2.transform.position.x, colGO2.transform.position.y, colGO2.transform.position.z);
+            col3Pos = new Vector3(colGO3.transform.position.x, colGO3.transform.position.y, colGO3.transform.position.z);
+            col4Pos = new Vector3(colGO4.transform.position.x, colGO4.transform.position.y, colGO4.transform.position.z);
+            #endregion
+
+            collider1 = ScriptableObject.CreateInstance<aaBB>() as aaBB;
+            collider2 = ScriptableObject.CreateInstance<aaBB>() as aaBB;
+            collider3 = ScriptableObject.CreateInstance<aaBB>() as aaBB;
+            collider4 = ScriptableObject.CreateInstance<aaBB>() as aaBB;
+
+            collider1._Init("A", col1Pos, 1);
+            collider2._Init("B", col2Pos, 1);
+            collider3._Init("C", col3Pos, 1);
+            collider4._Init("D", col4Pos, 1);
+
+            objects.Add(collider1);
+            objects.Add(collider2);
+            objects.Add(collider3);
+            objects.Add(collider4);
+        }
+
+        void FixedUpdate()
         {
-            colliderYPairs.Add(x.GetPairAsString());
-        });
-        #endregion
+            #region UpdateColliders
+            col1Pos = new Vector2(colGO1.transform.position.x, colGO1.transform.position.y);
+            col2Pos = new Vector2(colGO2.transform.position.x, colGO2.transform.position.y);
+            col3Pos = new Vector2(colGO3.transform.position.x, colGO3.transform.position.y);
+            col4Pos = new Vector2(colGO4.transform.position.x, colGO4.transform.position.y);
+
+            collider1._Update(col1Pos);
+            collider2._Update(col2Pos);
+            collider3._Update(col3Pos);
+            collider4._Update(col4Pos);
+            #endregion
+
+            //CollisionAB = util.TestOverLap(collider1, collider2);
+            //CollisionCD = util.TestOverLap(collider3, collider4);
+
+            #region SortAndSweep
+            colliderXPairs = null;
+            colliderXPairs = new List<string>();
+
+            colliderYPairs = null;
+            colliderYPairs = new List<string>();
+
+            colliderZPairs = null;
+            colliderZPairs = new List<string>();
+
+            util.SortandSweep(objects);
+            currentOBJSColliding = util.agentsColliding;
+
+            util.Xpairs.ForEach(x =>
+            {
+                colliderXPairs.Add(x.GetPairAsString());
+            });
+
+            util.Ypairs.ForEach(x =>
+            {
+                colliderYPairs.Add(x.GetPairAsString());
+            });
+
+            util.Zpairs.ForEach(x =>
+            {
+                colliderZPairs.Add(x.GetPairAsString());
+            });
+            #endregion
+        }
+
+        private void LateUpdate()
+        {
+            Debug.DrawLine(collider1.Min, collider1.Max, Color.green);
+            Debug.DrawLine(collider2.Min, collider2.Max, Color.green);
+            Debug.DrawLine(collider3.Min, collider3.Max, Color.green);
+            Debug.DrawLine(collider4.Min, collider4.Max, Color.green);
+        }
     }
 }
